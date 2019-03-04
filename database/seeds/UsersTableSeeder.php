ed9940cbf94d9e2e6dbd4ddc60ff1582efb196bd
Matthew Dixon <mdixon14717@gmail.com>
2019-03-04 16:05:55 +0000

    Add created/updated dates to the users table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -17,27 +18,37 @@ class UsersTableSeeder extends Seeder
             [
                 'name'     => 'Tony Stark',
                 'email'    => 'tony@test.com',
-                'password' => Hash::make('test1234')
+                'password' => Hash::make('test1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'name'     => 'Bruce Banner',
                 'email'    => 'bruce@test.com',
-                'password' => Hash::make('test1234')
+                'password' => Hash::make('test1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'name'     => 'Natalia Romanova',
                 'email'    => 'Natalia@test.com',
-                'password' => Hash::make('test1234')
+                'password' => Hash::make('test1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'name'     => 'Steve Rogers',
                 'email'    => 'steve@test.com',
-                'password' => Hash::make('test1234')
+                'password' => Hash::make('test1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ],
             [
                 'name'     => 'Thor Odinson',
                 'email'    => 'thor@test.com',
-                'password' => Hash::make('test1234')
+                'password' => Hash::make('test1234'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
             ]
         ]);
     }
